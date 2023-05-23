235af773e9e406d052e289915aa9a933873d5db3
George Godsmark <jorgestest2@gmail.com>
2023-05-23 12:52:27 -0600

    Initial commit

---
+++ SSL Echo Client/java/echoclient.java
@@ -0,0 +1,136 @@
+/*
+ * IPWorks SSL 2022 Java Edition- Demo Application
+ *
+ * Copyright (c) 2023 /n software inc. - All rights reserved. - www.nsoftware.com
+ *
+ */
+
+import java.io.*;
+
+import ipworksssl.*;
+
+public class echoclient extends ConsoleDemo {
+
+	private static Sslclient sslclient;
+
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.out.println("usage: echoclient server port");
+			System.out.println("");
+			System.out.println("  server  the address of the remote host");
+			System.out.println("  port    the TCP port in the remote host");
+			System.out.println("\r\nExample: echoclient localhost 777");
+		} else {
+			try {
+				sslclient = new Sslclient();
+				System.out.println("*************************************************************************************************************");
+				System.out.println("* This is a demo to show how to connect to a remote echo server, send data, and receive the echoed response.*");
+				System.out.println("*************************************************************************************************************\n");
+				sslclient.addSslclientEventListener(new DefaultSslclientEventListener() {
+					public void SSLServerAuthentication(SslclientSSLServerAuthenticationEvent e) {
+						e.accept = true;
+					}
+
+					public void connected(SslclientConnectedEvent e) {
+						System.out.println("\r\n" + sslclient.getRemoteHost() + " has connected.");
+						System.out.print(">");
+					}
+
+					public void dataIn(SslclientDataInEvent e) {
+						System.out.println("Received " + new String(e.text) + " from " + sslclient.getRemoteHost());
+						System.out.print(">");
+					}
+
+					public void disconnected(SslclientDisconnectedEvent e) {
+						System.out.println("Disconnected " + e.description + " from " + sslclient.getRemoteHost() + ".");
+						System.out.print(">");
+					}
+				});
+				sslclient.setRemoteHost(args[0]);
+				sslclient.setRemotePort(Integer.parseInt(args[1]));
+				sslclient.connect();
+				sslclient.doEvents();
+				if (sslclient.isConnected()) {
+					System.out.println("\r\nPlease input command: \r\n- 1 Send Data \r\n- 2 Exit");
+					System.out.print(">");
+
+					while (true) {
+						if (System.in.available() > 0) {
+							String command = String.valueOf(read());
+							if ("1".equals(command)) {
+								sslclient.setDataToSend(prompt("Please input sending data") + "\r\n");
+								System.out.println("Sending success.");
+								System.out.println("\r\nPlease input command: \r\n- 1 Send Data \r\n- 2 Exit");
+								System.out.print(">");
+							} else if ("2".equals(command)) {
+								break;
+							}
+						}
+					}
+					sslclient.disconnect();
+				} else {
+					System.out.println("\r\nCan't connect to server '" + args[0] + ":" + args[1] + "'");
+				}
+			} catch (Exception ex) {
+				System.out.println(ex.getMessage());
+			}
+		}
+	}
+}
+
+class ConsoleDemo {
+  private static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+  static String input() {
+    try {
+      return bf.readLine();
+    } catch (IOException ioe) {
+      return "";
+    }
+  }
+  static char read() {
+    return input().charAt(0);
+  }
+
+  static String prompt(String label) {
+    return prompt(label, ":");
+  }
+  static String prompt(String label, String punctuation) {
+    System.out.print(label + punctuation + " ");
+    return input();
+  }
+
+  static String prompt(String label, String punctuation, String defaultVal)
+  {
+	System.out.print(label + " [" + defaultVal + "] " + punctuation + " ");
+	String response = input();
+	if(response.equals(""))
+		return defaultVal;
+	else
+		return response;
+  }
+
+  static char ask(String label) {
+    return ask(label, "?");
+  }
+  static char ask(String label, String punctuation) {
+    return ask(label, punctuation, "(y/n)");
+  }
+  static char ask(String label, String punctuation, String answers) {
+    System.out.print(label + punctuation + " " + answers + " ");
+    return Character.toLowerCase(read());
+  }
+
+  static void displayError(Exception e) {
+    System.out.print("Error");
+    if (e instanceof IPWorksSSLException) {
+      System.out.print(" (" + ((IPWorksSSLException) e).getCode() + ")");
+    }
+    System.out.println(": " + e.getMessage());
+    e.printStackTrace();
+  }
+}
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,34 @@
+# Sample Projects
+
+## Requirements
+These sample projects require the latest version of IPWorks SSL.  Please download from:
+
+> Download: [https://www.nsoftware.com/ipworksssl](https://www.nsoftware.com/ipworksssl)
+
+## Sample Projects
+The sample projects demonstrate the usage of the IPWorks SSL components in a simple, 
+straightforward way.  They are not complete applications and they are not intended to be.
+Error handling and other checks are simplified for clarity.
+
+| Sample Project | Description |
+| --- | --- |
+| [Certificate Manager](./Certificate%20Manager) | Full-featured certificate management sample.  Demonstrates how to list, manage, create, and sign certificates. |
+| [FTP Client](./FTP%20Client) | A full featured FTP client built using the FTP component.  It allows browsing of directories, uploads and downloads of files, and more. |
+| [GET URL](./GET%20URL) | Shows how to download the content of a URL from a Web server. |
+| [HTML Mailer](./HTML%20Mailer) | Shows how to use HTMLMailer to send HTML Mail with inline images. |
+| [IMAP Email Client](./IMAP%20Email%20Client) | Shows how to use the IMAP component to access IMAP servers (e.g. MS Exchange). |
+| [LDAP Search](./LDAP%20Search) | Shows how to contact an LDAP server and lookup a name. |
+| [OAuth](./OAuth) | Demonstrates how to authenticate using the OAuth component. |
+| [POP Email Client](./POP%20Email%20Client) | A simple email client. It shows how to use the SMTP and POP-based components to access POP and SMTP mail servers. |
+| [REST OpenWeatherAPI](./REST%20OpenWeatherAPI) | Shows how to use the REST object to send and retrieve information from RESTful web services. |
+| [SOAP Temperature Converter](./SOAP%20Temperature%20Converter) | Uses the SOAP component and a freely available Web Service to do temperature conversion. |
+| [SSL Echo Client](./SSL%20Echo%20Client) | A simple SSL echo client.  It demonstrates the use of SSLClient to build the simplest TCP/IP client.  Use in conjunction with the SSL Echo Server demo. |
+| [SSL Echo Server](./SSL%20Echo%20Server) | A simple SSL echo server.  It demonstrates the use of SSLServer to build the simplest TCP/IP server.  Use in conjunction with the SSL Echo Client demo. |
+| [Telnet Client](./Telnet%20Client) | A simple demo of the Telnet component.  Shows how to use the component to build a Telnet client. |
+| [WebDAV Client](./WebDAV%20Client) | Allows you to copy files back and forth between a DAV server and localhost. |
+| [WebSocket Client](./WebSocket%20Client) | Shows how to use the WebSocketClient component to send and receive data. |
+| [WebSocket Server](./WebSocket%20Server) | Uses the WebSocketServer component to create a simple WebSocket server. |
+
+## Support
+If you have questions or need help, please contact support@nsoftware.com or explore other support options 
+at www.nsoftware.com.
